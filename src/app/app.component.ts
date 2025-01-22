cdcba8c2df5c207ecf288b4729aa4f678af015e6
Raluca <spatarularisaraluca@gmail.com>
2025-01-22 19:26:58 +0200

    Add main component with styles and integrate into app layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { MenuComponent } from './components/menu/menu.component';
 import { FooterComponent } from "./components/footer/footer.component";
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
+import { MainComponent } from "./components/main/main.component";
 
 @Component({
   selector: 'app-root',
-  imports: [MenuComponent, FooterComponent, SidebarComponent],
+  imports: [MenuComponent, FooterComponent, SidebarComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
