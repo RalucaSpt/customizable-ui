55748bf59a79fe8738e04e1d20dc50c07a6f6726
Raluca <spatarularisaraluca@gmail.com>
2025-01-31 16:39:27 +0200

    Refactor navigation

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -2,12 +2,13 @@ import { Component, OnInit, effect, inject, signal } from '@angular/core';
 import { ConfigService } from '../../services/configService';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css'],
-  imports: [CommonModule, MatIconModule]
+  imports: [CommonModule, MatIconModule, RouterLink]
 })
 export class MenuComponent {
   private configService = inject(ConfigService); 
@@ -29,8 +30,7 @@ export class MenuComponent {
     console.log('sidebarExpanded:', this.sidebarExpanded());
   }
 
-  onMenuItemClick(item: string) {
-    this.currentPage.set(item);
-    console.log('currentPage:', this.currentPage());
+  formatRoute(name: string): string {
+    return name.toLowerCase().replace(/\s+/g, '-');
   }
 }
