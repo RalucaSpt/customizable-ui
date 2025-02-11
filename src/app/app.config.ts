35330e6381632cb704052ceb1a184f1ffa1a5d75
Raluca <spatarularisaraluca@gmail.com>
2025-02-11 18:29:00 +0200

    Add translation loader configuration to app.config for ngx-translate support

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,23 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes'; 
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, HttpClient } from '@angular/common/http';
+import { TranslateLoader, TranslateService } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, 'assets/translations/', '.json');
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes), 
-    provideHttpClient(), 
+    provideHttpClient(),
+    TranslateService,
+    {
+      provide: TranslateLoader,
+      useFactory: HttpLoaderFactory,
+      deps: [HttpClient]
+    }
   ],
 };
