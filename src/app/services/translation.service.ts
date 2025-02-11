2b29d66f8379af429a5039c1045869fe0532a1bf
Raluca <spatarularisaraluca@gmail.com>
2025-02-11 18:22:24 +0200

    Enhance TranslationService to support dynamic language switching and default language loading

--- src/app/services/translation.service.ts
+++ src/app/services/translation.service.ts
@@ -1,9 +1,32 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TranslationService {
+  private translate = inject(TranslateService);
 
-  constructor() { }
+  constructor() {
+    this.translate.setDefaultLang('en');
+    this.loadLanguage();
+  }
+
+  setLanguage(lang: string) {
+    this.translate.use(lang);
+    localStorage.setItem('lang', lang);
+  }
+
+  getLanguage(): string {
+    return this.translate.currentLang;
+  }
+
+  private loadLanguage() {
+    const savedLang = localStorage.getItem('lang') || 'en';
+    this.translate.use(savedLang);
+  }
+
+  translateKey(key: string): string {
+    return this.translate.instant(key);
+  }
 }
