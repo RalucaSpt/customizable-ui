346f92ad07b7a8643c8fc0a4b36123c7c2b3c993
Raluca <spatarularisaraluca@gmail.com>
2025-02-11 22:14:14 +0200

    Refactor footer component for improved styling and social links integration

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,15 +1,18 @@
 import { Component, effect, inject, OnInit } from '@angular/core';
 import { ConfigService } from '../../services/configService';
 import { CommonModule } from '@angular/common';
+import { TranslationService } from '../../services/translation.service';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css'],
-  imports: [CommonModule]
+  imports: [CommonModule, MatIconModule]
 })
 export class FooterComponent{
   configService = inject(ConfigService); 
+  translationService = inject(TranslationService);
 
   footerConfig = this.configService.footerConfig;
 
