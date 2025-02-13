1ce44db2f3287ef2baa190600c74e8a16aeb411e
Raluca <spatarularisaraluca@gmail.com>
2025-02-13 12:30:33 +0200

    Add authentication component with form for sign up and login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
 import { ServicesComponent } from './pages/services/services.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { AuthComponent } from './auth/auth.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about-us', component: AboutUsComponent },
   { path: 'services', component: ServicesComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'auth', component: AuthComponent },
   { path: '**', redirectTo: '' }, 
 ];
